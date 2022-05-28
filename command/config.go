a14e1b9c034acf4a12e2d26701a056a9e79d2220
Stacey Brogden <toad@brothertoad.net>
2022-05-28 12:15:05 -0700

    Use an anonymous struct for configuration data.

--- command/config.go
+++ command/config.go
@@ -5,11 +5,9 @@ import (
   "github.com/urfave/cli/v2"
 )
 
-type Config struct {
+var config struct {
   dir string
 }
-
-var config Config
 
 func LoadConfig(c *cli.Context) error {
   fmt.Printf("Loading configuration from %s...\n", c.String("config"))
