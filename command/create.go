86a7463b48d039c8cac1a60428f1b00f00d83ae6
Stacey Brogden <toad@brothertoad.net>
2022-05-28 19:55:27 -0700

    Add translations and check for missing tags.

--- command/create.go
+++ command/create.go
@@ -4,8 +4,8 @@ import (
   "fmt"
   "io/fs"
   "log"
-  "os"
   "path/filepath"
+  "strings"
   "github.com/urfave/cli/v2"
   "github.com/brothertoad/musiclib/common"
   "github.com/brothertoad/musiclib/tags"
@@ -17,8 +17,28 @@ var CreateCommand = cli.Command {
   Action: doCreate,
 }
 
+// Perhaps use constants from commom for target keys.
 var keyTranslations = map[string]string {
-  "xyz": "abc",
+  "\xa9nam": common.TitleKey,
+  "\xa9ART" : common.ArtistKey,
+  "\xa9alb" : common.AlbumKey,
+  "soar" : common.ArtistSortKey,
+  "soal" : common.AlbumSortKey,
+  "ALBUM" : common.AlbumKey,
+  "ARTIST" : common.ArtistKey,
+  "TITLE" : common.TitleKey,
+  "trkn" : common.TrackNumberKey,
+  "disk" : common.DiscNumberKey,
+  "tracknumber" : common.TrackNumberKey,
+  "TRACKNUMBER" : common.TrackNumberKey,
+  "DISKNUMBER" : common.DiscNumberKey,
+  "TIT2" : common.TitleKey,
+  "TPE1" : common.ArtistKey,
+  "TALB" : common.AlbumKey,
+}
+
+var requiredKeys = []string {
+  common.TitleKey, common.ArtistKey, common.AlbumKey, common.TrackNumberKey, common.DiscNumberKey,
 }
 
 var songs []common.Song
@@ -28,9 +48,7 @@ func doCreate(c *cli.Context) error {
   if len(config.MusicDir) == 0 {
     log.Fatalln("No top level directory specified in configuration.")
   }
-  if _, err := os.Stat(config.MusicDir); os.IsNotExist(err) {
-  	log.Fatalf("Top level directory '%s' does not exist.\n", config.MusicDir)
-  }
+  dirMustExist(config.MusicDir)
   songs = make([]common.Song, 5000)
   filepath.WalkDir(config.MusicDir, loadFile)
   return nil
@@ -40,13 +58,58 @@ func loadFile(path string, de fs.DirEntry, err error) error {
   if de.IsDir() {
     return nil
   }
-  m := tags.GetTagsFromFile(path)
-  if m == nil || len(m) == 0 {
+  song := tags.GetTagsFromFile(path)
+  if song == nil || len(song) == 0 {
     return nil
   }
-  // Standardize keys.
+  song[common.PathKey] = path
+  translateKeys(song)
   // Add sort keys.
-  // Add flags.
-  songs = append(songs, m)
+  song[common.FlagsKey] = ""
+  checkForMissingKeys(song)
+  songs = append(songs, song)
   return nil
+}
+
+// Replace keys with standard names.
+func translateKeys(song common.Song) {
+  for k, v := range song {
+    if trans, present := keyTranslations[k]; present {
+      delete(song, k)
+      song[trans] = v
+    }
+  }
+  // Check for the track number.  If it doesn't exist, see if it has the TRCK tag, which
+  // is track number / track total and get the track number from that.
+  if _, present := song[common.TrackNumberKey]; !present {
+    if tntt, tnttPresent := song["TRCK"]; tnttPresent {
+      s := strings.Split(tntt, "/")
+      song[common.TrackNumberKey] = s[0]
+    } else {
+      log.Printf("Can't get track number for '%s'\n", song[common.PathKey])
+    }
+  }
+  // Check for the disc number.  If it doesn't exist, see if it has the TPOS tag, which
+  // is disc number / disc total and get the disc number from that.  If that doesn't
+  // exist, assume disc 1.
+  if _, present := song[common.DiscNumberKey]; !present {
+    if dndt, dndtPresent := song["TPOS"]; dndtPresent {
+      s := strings.Split(dndt, "/")
+      song[common.DiscNumberKey] = s[0]
+    } else {
+      song[common.DiscNumberKey] = "1"
+    }
+  }
+}
+
+func addSortKeys(song common.Song) {
+
+}
+
+func checkForMissingKeys(song common.Song) {
+  for _, k := range(requiredKeys) {
+    if _, present := song[k]; !present {
+      fmt.Printf("%+v is missing %s\n", song, k)
+    }
+  }
 }
--- common/common.go
+++ common/common.go
@@ -1,7 +1,16 @@
 package common
 
+const PathKey = "path"
+const TitleKey = "title"
+const ArtistKey = "artist"
+const AlbumKey = "album"
+const TrackNumberKey = "trackNumber"
+const DiscNumberKey = "discNumber"
+const ArtistSortKey = "artistSort"
+const AlbumSortKey = "albumSort"
 const DurationKey = "duration"
 const MimeKey = "mime"
 const ExtensionKey = "extension"
+const FlagsKey = "flags"
 
 type Song map[string]string
