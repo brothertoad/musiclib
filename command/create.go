bcc3fe4cb043b27c535b0b4d2891c2976d9d29c5
Stacey Brogden <toad@brothertoad.net>
2022-06-07 15:03:34 -0700

    More refactoring.

--- command/create.go
+++ command/create.go
@@ -1,21 +1,12 @@
 package command
 
 import (
-  _ "encoding/hex"
   "fmt"
-  _ "hash"
-  _ "io"
-  "io/fs"
   "io/ioutil"
-  _ "log"
-  _ "os"
-  "path/filepath"
   "sort"
-  _ "strings"
   "github.com/urfave/cli/v2"
   "gopkg.in/yaml.v3"
   "github.com/brothertoad/musiclib/common"
-  "github.com/brothertoad/musiclib/tags"
 )
 
 const saveFlag = "save"
@@ -33,33 +24,16 @@ var CreateCommand = cli.Command {
 
 func doCreate(c *cli.Context) error {
   fmt.Printf("Creating database from directory %s...\n", config.MusicDir)
-  // save the length, as we need it to remove the prefix of each file
-  songMaps := make(common.SongMapSlice, 0, 5000)
+  var songMaps common.SongMapSlice
 
   // If the load flag was specified, load from a file, rather than walking
   // through the entire music directory.
   if len(c.String(loadFlag)) > 0 {
-    loadSongs(c.String(loadFlag), songMaps)
+    songMaps = loadSongsFromYaml(c.String(loadFlag))
   } else {
-    filepath.WalkDir(config.MusicDir, func(path string, de fs.DirEntry, err error) error {
-      if de.IsDir() {
-        return nil
-      }
-      song := tags.GetTagsFromFile(path)
-      if song == nil || len(song) == 0 {
-        return nil
-      }
-      setPaths(song, path)
-      song[common.FlagsKey] = common.EncodeFlag
-      translateKeys(song)
-      addSortKeys(song)
-      addMd5Key(song)
-      checkForMissingKeys(song)
-      songMaps = append(songMaps, filterKeys(song))
-      return nil
-    })
-    sort.Sort(songMaps)
+    songMaps = loadSongMapSliceFromMusicDir()
   }
+  sort.Sort(songMaps)
 
   // Save if the save flag was specified.
   if len(c.String(saveFlag)) > 0 {
@@ -75,10 +49,12 @@ func doCreate(c *cli.Context) error {
   return nil
 }
 
-func loadSongs(path string, songMaps common.SongMapSlice) {
+func loadSongsFromYaml(path string) common.SongMapSlice {
+  songMaps := make(common.SongMapSlice, 0, 5000)
   // logic came from https://zetcode.com/golang/yaml/
   yfile, err := ioutil.ReadFile(path)
   checkError(err)
   err2 := yaml.Unmarshal(yfile, &songMaps)
   checkError(err2)
+  return songMaps
 }
