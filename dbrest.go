b789416a023f0cb4470c2779b1e1b244e28620c3
Stacey Brogden <toad@brothertoad.net>
2024-04-24 12:15:20 -0700

    Make default state 100, make get all work.

--- dbrest.go
+++ dbrest.go
@@ -6,14 +6,25 @@ import (
 
 func loadArtistsByState(db *sql.DB, state int) ([]ArtistModel, error) {
   resp := make([]ArtistModel, 0)
-  stmt, err := db.Prepare("select id, name from artists where exists " +
-    "(select * from albums where albums.artist = artists.id and exists " +
-      "(select * from songs where songs.album = albums.id and state = $1)) order by sort_name")
+  var stmt *sql.Stmt
+  var err error
+  if state != 0 {
+    stmt, err = db.Prepare("select id, name from artists where exists " +
+      "(select * from albums where albums.artist = artists.id and exists " +
+        "(select * from songs where songs.album = albums.id and state = $1)) order by sort_name")
+  } else {
+    stmt, err = db.Prepare("select id, name from artists order by sort_name")
+  }
   if err != nil {
     return resp, err
   }
   defer stmt.Close()
-  rows, err := stmt.Query(state)
+  var rows *sql.Rows
+  if state != 0 {
+    rows, err = stmt.Query(state)
+  } else {
+    rows, err = stmt.Query()
+  }
   if err != nil {
     return resp, err
   }
