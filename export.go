5d059ba3fa602dbfde2fa25db404d036f54828c8
Stacey Brogden <toad@brothertoad.net>
2025-12-10 11:10:16 -0800

    Create one list of songs when exporting.

--- export.go
+++ export.go
@@ -22,9 +22,21 @@ var exportCommand = cli.Command {
   Action: doExport,
 }
 
+type songInfo struct {
+  artist, album string
+  disc, track int
+  title string
+}
+
 func doExport(c *cli.Context) error {
   db := getDbConnection()
   defer db.Close()
+
+  // Create a slice of songInfo, and put all the songs in it, sorted.
+  // This allows us to find the longest artist, album and title, so
+  // we can print out the song list more cleanly.
+  songInfoList := make([]songInfo, 0, 10000)
+
   artists := slices.Collect(maps.Values(readArtistMapFromDb(db)))
   // fmt.Printf("%d artists are candidates for exporting\n", len(artists))
   sort.Slice(artists, func(i, j int) bool {
@@ -48,9 +60,18 @@ func doExport(c *cli.Context) error {
         return songs[i].TrackNumber < songs[j].TrackNumber
       })
       for _, song := range(songs) {
-        fmt.Printf("%s   %s   %d  %d  %s\n", artist.Name, album.Title, song.DiscNumber, song.TrackNumber, song.Title)
+        var info songInfo
+        info.artist = artist.Name
+        info.album = album.Title
+        info.disc = song.DiscNumber
+        info.track = song.TrackNumber
+        info.title = song.Title
+        songInfoList = append(songInfoList, info)
       }
     }
+  }
+  for _, info := range(songInfoList) {
+    fmt.Printf("%s   %s   %d  %d  %s\n", info.artist, info.album, info.disc, info.track, info.title)
   }
   return nil
 }
